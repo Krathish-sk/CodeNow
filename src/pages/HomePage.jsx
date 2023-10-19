c16f4ab10cfb40ad8d97b9bec898a463395d6329
Krathish K <krathishs.k123@gmail.com>
2023-10-19 16:56:22 +0530

    create & join room functionality

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { v4 as uuidV4 } from "uuid";
 import toast from "react-hot-toast";
+import "./homePage.css";
 
 export default function HomePage() {
   const navigate = useNavigate();
@@ -9,18 +10,41 @@ export default function HomePage() {
   const [roomId, setRoomId] = useState("");
   const [username, setUsername] = useState("");
 
-  function createNewRoom(e) {}
+  function createNewRoom(e) {
+    const id = uuidV4();
+    setRoomId(id);
+    toast.success("New room created. Join now !");
+  }
 
-  function joinRoom() {}
+  function joinRoom(e) {
+    e.preventDefault();
+    if (!roomId) {
+      toast.error("Room ID is mandatory !");
+      return;
+    } else if (!username) {
+      toast.error("Username is mandatory !");
+      return;
+    }
+
+    // Redirect
+    navigate(`/editor/${roomId}`, {
+      state: {
+        username,
+      },
+    });
+  }
 
   return (
     <div className="homePageWrapper">
       <main className="formWrapper">
-        <img
-          src="CodeNow-Logo.png"
-          alt="CodeNow-Logo"
-          className="homePageLogo"
-        />
+        <div className="logoDiv">
+          <img
+            src="CodeNow-Logo.png"
+            alt="CodeNow-Logo"
+            className="homePageLogo"
+            loading="lazy"
+          />
+        </div>
         <form onSubmit={joinRoom} className="inputGroup">
           <input
             type="text"
@@ -36,17 +60,26 @@ export default function HomePage() {
             onChange={(e) => setUsername(e.target.value)}
           />
           <button className="btn joinBtn" type="submit">
-            Code Now
+            Code-Now
           </button>
           <span className="createInfo">
-            No room ID ? <span className="createNewBtn">Create room</span>
+            No room ID ?{" "}
+            <span className="createNewBtn" onClick={createNewRoom}>
+              Create room
+            </span>
           </span>
         </form>
       </main>
       <footer>
         <h4>
-          Built with ❤️ &nbsp; by &nbsp;{" "}
-          <a href="https://github.com/Krathish-sk/CodeNow">KK</a>
+          Built with ❤️&nbsp;by&nbsp;
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/Krathish-sk/CodeNow"
+          >
+            KK
+          </a>
         </h4>
       </footer>
     </div>
